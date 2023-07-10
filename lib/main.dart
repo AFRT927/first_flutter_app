328fef70f0798051c0342d1eacf8552e64f0a0d1
Andres <andrese-rios@utp.edu.co>
2023-07-10 14:16:19 -0500

    add image from local dir

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class Home extends StatelessWidget {
     ),
   body: const Center(
     child: Image(
-      image: NetworkImage('https://images.unsplash.com/photo-1608754482805-6f630357358b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3BhY2UlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww&w=1000&q=80'),
+      image: AssetImage('assets/space2.jpeg'),
     ),
   ),
   floatingActionButton: FloatingActionButton(
