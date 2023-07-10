cd4cf2e8826aa2c10cd0af9a03718c11c5e1b828
Andres <andrese-rios@utp.edu.co>
2023-07-10 11:44:54 -0500

    Colours and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: const Text('my first app'),
       centerTitle: true,
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.red[600],
     ),
   body: const Center(
-    child: Text('hello ninjas'),
+    child: Text(
+      'hello ninjas',
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color: Colors.grey,
+        fontFamily: AutofillHints.creditCardFamilyName
+      ),
+    ),
+  ),
+  floatingActionButton: FloatingActionButton(
+    onPressed: () {},
+    child: const Text('click'),
+    backgroundColor: Colors.red[600],
   ),
   ),
 ));
