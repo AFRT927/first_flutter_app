f42c6007fba89fb68398cfb6b4869fdc07fd50dc
Andres <andrese-rios@utp.edu.co>
2023-07-10 13:50:02 -0500

    stateless widget and hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
+  home: const Home(),
+));
+
+/**
+ * stateless Widgets:
+ * the state of the widget cannot change over time
+ * puede contener Data, pero esa data no puede
+ * cambiar despues de que el widget ha sido
+ * inicializado
+ * 
+ * stateful widgets:
+ * the state of the widget can change over time
+ * cualquier dato dentro del widget puede cambiar
+ */
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     appBar: AppBar(
       title: const Text('my first app'),
       centerTitle: true,
@@ -24,5 +43,6 @@ void main() => runApp(MaterialApp(
     child: const Text('click'),
     backgroundColor: Colors.red[600],
   ),
-  ),
-));
+  );
+  }
+}
